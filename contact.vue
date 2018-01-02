7ce29426a5f9d775da736a9c5247eccb52608a2e
Rich Snapp <rich.snapp@gmail.com>
2018-01-01 22:42:34 -0700

    8601230

---
+++ contact.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="btn-group btn-group-justified form-group social-links visible-xs visible-sm" role="group">
+      <a href="https://www.facebook.com/rich.snapp" target="_blank" class="btn fa fa-facebook"></a>
+      <a href="https://twitter.com/snapwich" target="_blank" class="btn fa fa-twitter"></a>
+      <a href="https://github.com/snapwich" target="_blank" class="btn fa fa-github-alt"></a>
+      <a href="http://www.linkedin.com/in/richsnapp" target="_blank" class="btn fa fa-linkedin"></a>
+    </div>
+    <p>Feel free to contact by clicking the social icons above or using the form below. Be sure to include a return
+      email address or some form of contact information in the message body if you'd appreciate a response.</p>
+    <form name="contact" novalidate class="contact-form" v-on:submit.prevent>
+      <div v-if="!submitted">
+        <div class="row">
+          <div class="form-group col-sm-6" :class="{'has-error': submitAttempted && errors.has('name')}">
+            <label class="control-label">Name</label>
+            <input name="name" class="form-control" v-model="name" type="text" placeholder="Your name"
+                   v-validate="{required: true}" />
+          </div>
+          <div class="form-group col-sm-6" :class="{'has-error': submitAttempted && errors.has('email')}">
+            <label class="control-label">Email</label>
+            <input name="email" class="form-control" v-model="email" type="email"
+                   placeholder="Your email address" v-validate="{required: true, email: true}" />
+          </div>
+        </div>
+        <div class="form-group" :class="{'has-error': submitAttempted && errors.has('message')}">
+          <label class="visible-xs control-label">Message</label>
+          <textarea name="message" class="form-control" v-model="message"
+                    placeholder="Your message of subtle adoration or admonishment"
+                    v-validate="{required: true}"></textarea>
+        </div>
+        <div class="row">
+          <div class="form-group form-group col-sm-8"
+               :class="{'has-error': submitAttempted && errors.has('recaptcha')}">
+
+          </div>
+          <div class="form-group col-sm-4">
+            <vue-recaptcha ref="captcha" @verify="onVerify" sitekey="6LdL9T4UAAAAALj9ztC5O1Unlh1Y-Mf8OjFBUpo0">
+              <input type="submit" class="btn btn-default"/>
+            </vue-recaptcha>
+          </div>
+        </div>
+      </div>
+      <div class="alert alert-danger" v-for="error in errorMessages">
+        <strong>{{error.name}}</strong>: {{error.message}}
+      </div>
+      <div class="alert alert-danger" v-if="submitAttempted && errors.any()" role="alert">
+        Please correct the marked fields and try again.
+      </div>
+      <div class="alert alert-success" v-if="submitted" role="alert">
+        Your message has been successfully sent.
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import VeeValidate from 'vee-validate';
+  import VueRecaptcha from 'vue-recaptcha';
+  import axios from 'axios';
+  import * as _ from 'lodash';
+
+  Vue.use(VeeValidate);
+
+  export default {
+    head() {
+      return {
+        script: [
+          { src: "https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" }
+        ]
+      }
+    },
+    data() {
+      return {
+        name: null,
+        email: null,
+        message: null,
+        recaptcha: false,
+        submitted: false,
+        submitAttempted: false,
+        errorMessages: []
+      };
+    },
+    methods: {
+      onVerify(recaptcha) {
+        this.submitAttempted = true;
+        this.$validator.validateAll().then((valid) => {
+          if (valid) {
+            axios.post('http://api.richsnapp.com/mail', {
+              name: this.name,
+              email: this.email,
+              message: this.message,
+              recaptcha
+            }).then((result) => {
+              this.submitted = true;
+            }).catch((err) => {
+              let message = _.get(err, 'data.message');
+              if (!message) {
+                message = 'There was an error submitting your message'
+              }
+
+              this.$refs.captcha.reset();
+
+              if (err.status === 'fail') {
+                _.forEach(err.data.data).forEach((message, name) => {
+                  this.errorMessages.push({name, message});
+                })
+              } else {
+                this.submitAttempted = false;
+                this.errorMessages.push({name: "error", message});
+              }
+            });
+          } else {
+            this.$refs.captcha.reset();
+          }
+        });
+      }
+    },
+    components: {
+      VueRecaptcha
+    }
+  }
+</script>
---
+++ index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p>Rich Snapp is a Software Engineer focusing on web application development and is involved in both
+      building and pushing forward the capabilities of software designed for the web. His efforts are
+      currently concentrated in Javascript programming and front-end frameworks, but he has a strong
+      familiarity with the full technology stack necessary for creating modern <a
+        href="https://en.wikipedia.org/wiki/Software_as_a_service" target="_blank">SaaS</a> applications.
+    </p>
+    <figure v-toggle-class-on-click="'clicked'">
+      <img src="/img/responsive-workstation.jpg"/>
+      <figcaption>Where the magic happens.</figcaption>
+    </figure>
+    <p>Rich currently resides in Salt Lake City, Utah and works at <a href="https://www.needle.com/"
+                                                                      target="_blank">Needle</a>,
+      spearheading
+      <a href="https://en.wikipedia.org/wiki/Responsive_web_design" target="_blank">responsive design</a>
+      (mobile/tablet friendly) in <a href="https://www.needle.com/responsive/" target="_blank">Needle's
+        advocacy based chat platform</a>. He also holds a Bachelor of Science in Computer Science from <a
+        href="http://www.uvu.edu/" target="_blank">Utah Valley University</a>.</p>
+    <p>More information on his skills and experience can be found at his <a
+      href="https://www.linkedin.com/in/richsnapp" target="_blank">LinkedIn page</a>.</p>
+    <h3>About This Site</h3>
+    <p>As an open-source software contributor and consumer, as well as appreciator of free services; here's
+      a
+      few notes concerning the making of this website:</p>
+    <ul>
+      <li>This is a <a href="https://en.wikipedia.org/wiki/Static_web_page" target="_blank">static web
+        page</a> generated with <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a> and various other
+        <a
+          href="https://www.npmjs.com/" target="_blank">npm libraries</a>.
+      </li>
+      <li>Hosted with <a href="https://pages.github.com/" target="_blank">GitHub Pages</a></li>
+      <li>Uses some <a href="http://getbootstrap.com/" target="_blank">Twitter Bootstrap</a> components
+        (such
+        as the grid) and <a href="https://fortawesome.github.io/Font-Awesome/" target="_blank">Font
+          Awesome</a> icons.
+      </li>
+      <li>Type fonts provided by <a href="https://www.google.com/fonts" target="_blank">Google Fonts</a>
+      </li>
+      <li>Intentionally made as simple and unparallaxy as possible, <a
+        href="http://www.quotationspage.com/quote/39411.html" target="_blank">sorry for the convenience</a>.
+      </li>
+    </ul>
+  </div>
+</template>
