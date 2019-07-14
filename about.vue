1fe38d949fa74b0454cb9683aac33e74db1d0e52
Rich Snapp <rich.snapp@gmail.com>
2019-07-14 16:29:18 -0600

    update about page

--- about.vue
+++ about.vue
@@ -27,8 +27,5 @@
   * Hosted with [Netlify](https://www.netlify.com/)
   * Using some [Twitter Bootstrap](https://getbootstrap.com/) components (such as the grid) and
     [Font Awesome](https://fortawesome.github.io/Font-Awesome/) icons.
-  * Share functionality by [ShareThis](https://www.sharethis.com).
-  * Type fonts by [Google Fonts](https://www.google.com/fonts).
-  * Comments using the [Facebook Social Comments](https://developers.facebook.com/products/social-plugins/comments/) plugin.
   * Intentionally made as simple and unparallaxy as possible.
 </template>
