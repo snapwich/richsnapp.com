15fda153f20135746e66dbc7b3a96764749bc7cc
Rich Snapp <rich.snapp@gmail.com>
2023-05-29 10:18:51 -0600

    create share / bookmark link for lazy-portfolio tool

--- tools/lazy-portfolio.vue
+++ tools/lazy-portfolio.vue
@@ -1,13 +1,20 @@
 <template>
-  <client-only>
-    <lazy-portfolio
-      :currencies="currencies"
-      :funds="funds"
-      :portfolios="portfolios"
-      :startingPortfolio="startingPortfolio"
-      :urlEncodedValue.sync="encodedStr"
-    />
-  </client-only>
+  <div>
+    <client-only>
+      <lazy-portfolio
+        :currencies="currencies"
+        :funds="funds"
+        :portfolios="portfolios"
+        :startingPortfolio="startingPortfolio"
+        :urlEncodedValue.sync="encodedStr"
+      />
+      <div class="links">
+        <a class="share-link" :href="shareLink"
+          >share / bookmark link to current portfolio</a
+        >
+      </div>
+    </client-only>
+  </div>
 </template>
 
 <script lang="ts">
@@ -52,37 +59,52 @@ export default defineComponent({
 
     let encodedStr = computed({
       get() {
-        if (global.location) {
-          let url = new URL(global.location.href);
-          if (url.searchParams.has("p")) {
-            return url.searchParams.get("p");
-          }
-        }
         return storage.get<string>("encodedStr") || "";
       },
       set(val) {
-        if (
-          global.history &&
-          global.location &&
-          // don't update the url if it's the default value
-          val !==
-            "WyJVU0QiLDAsW1siVG90YWwgQm9uZCIsNDAsMCxmYWxzZV0sWyJUb3RhbCBTdG9jayIsNDAsMCxmYWxzZV0sWyJUb3RhbCBJbnRlcm5hdGlvbmFsIFN0b2NrIiwyMCwwLGZhbHNlXV1d"
-        ) {
-          let url = new URL(global.location.href);
-          url.searchParams.set("p", val || "");
-          global.history.replaceState(null, "", url);
-        }
         storage.set("encodedStr", val);
       }
     });
+
+    let shareLink = computed(() => {
+      if (global.location) {
+        let url = new URL(global.location.href);
+        url.searchParams.set("p", encodedStr.value || "");
+        return url.toString();
+      }
+      return "";
+    });
+
+    if (global.location) {
+      let url = new URL(global.location.href);
+      let p = url.searchParams.get("p");
+      if (p) {
+        encodedStr.value = p;
+      }
+    }
 
     return {
       currencies,
       funds,
       portfolios,
       encodedStr,
+      shareLink,
       startingPortfolio: "Rick Ferri Lazy 40/40/20"
     };
   }
 });
 </script>
+
+<style>
+.links {
+  display: flex;
+  justify-content: right;
+}
+.share-link {
+  text-align: center;
+  text-decoration: none;
+  color: inherit;
+  border-bottom: 2px solid #b7dee6;
+  box-shadow: inset 0 -3px 0 #b7dee6;
+}
+</style>
