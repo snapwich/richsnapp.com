25bb771111a918da6f9516fafba6657271d8d6cb
Rich Snapp <rich.snapp@gmail.com>
2024-08-25 23:23:44 -0600

    well, we're stuck. astro doesn't support components in content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
-import remarkExternalLinks from "remark-external-links";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://richsnapp.com",
   integrations: [vue(), mdx()],
   markdown: {
-    remarkPlugins: [
-      [remarkExternalLinks, { target: "_blank", rel: ["noopener"] }],
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["noopener"] }],
     ],
   },
 });
