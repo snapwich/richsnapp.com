f541990ceadfebe8b6165e714d23f16e075c53fd
Rich Snapp <rich.snapp@gmail.com>
2024-12-27 14:44:20 -0700

    refactor slug and render for astro v4 -> v5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,27 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  type: "content",
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
     tldr: z.array(z.string()).optional(),
   }),
+  loader: glob({
+    pattern: ["**/*.mdx", "!**/node_modules/**"],
+    base: "./src/content/blog",
+  }),
 });
 
 const article = defineCollection({
-  type: "content",
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
     tldr: z.array(z.string()).optional(),
+  }),
+  loader: glob({
+    pattern: ["**/*.mdx", "!**/node_modules/**"],
+    base: "./src/content/article",
   }),
 });
 
